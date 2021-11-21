46dc6a53fba11544f2dff38437e759b020e3b98f
Ross Mawdsley <rossmawd@gmail.com>
2021-11-21 20:03:14 +0000

    06- forms extra. 1st attempt

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,33 +2,19 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
+import {useRef} from 'react'
 
-function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
-
+function UsernameForm({onSubmitUsername, myRef}) {
   const handleSubmit = (event) => {
     event.preventDefault()
-    onSubmitUsername(event.target.elements[0].value)
+    onSubmitUsername(myRef.current.value)
   }
 
-  // 🐨 add the onSubmit handler to the <form> below
-
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="my-input">Username:</label>
-        <input type="text" id="my-input"/>
+        <input ref={myRef} type="text" id="my-input"/>
       </div>
       <button type="submit">Submit</button>
     </form>
@@ -36,8 +22,10 @@ function UsernameForm({onSubmitUsername}) {
 }
 
 function App() {
+  const myRef = useRef();
+
   const onSubmitUsername = username => alert(`You entered: ${username}`)
-  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+  return <UsernameForm onSubmitUsername={onSubmitUsername} myRef={myRef} />
 }
 
 export default App
