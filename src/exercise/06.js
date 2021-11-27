ebade2519f6d31cab2d45e4c1eee2aeb91cd9af8
Ross Mawdsley <rossmawd@gmail.com>
2021-11-27 14:31:36 +0000

    06-02 using state to show error message

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,24 +5,35 @@ import * as React from 'react'
 import {useRef} from 'react'
 
 function UsernameForm({onSubmitUsername, myRef}) {
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault()
     onSubmitUsername(myRef.current.value)
+  }
+
+  const [errorState, setErrorState] = React.useState(null)
+
+  const handleChange = event => {
+    let input = event.target.value
+    const isValid = input === input.toLowerCase()
+    setErrorState(isValid ? null : 'Username must be lower case')
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="my-input">Username:</label>
-        <input ref={myRef} type="text" id="my-input"/>
+        <input onChange={handleChange} ref={myRef} type="text" id="my-input" />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={errorState}>
+        Submit
+      </button>
+      {errorState ? <p role="alert">{errorState}</p> : null}
     </form>
   )
 }
 
 function App() {
-  const myRef = useRef();
+  const myRef = useRef()
 
   const onSubmitUsername = username => alert(`You entered: ${username}`)
   return <UsernameForm onSubmitUsername={onSubmitUsername} myRef={myRef} />
